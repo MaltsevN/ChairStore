70da341293388de218be236a31df2156b253a1de
Nikita Maltsev <nikitamalcev1@gmail.com>
2017-03-06 00:35:43 +0200

    Added category menu

---
+++ Domain/Concrete/EFChairRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Abstract;
+using Domain.Entities;
+using Domain.Concrete;
+
+namespace Domain.Concrete
+{
+    public class EFChairRepository : IChairRepository
+    {
+        EFDBContext context = new EFDBContext();
+        public IEnumerable<Chair> Chairs
+        {
+            get
+            {
+                return context.Chairs;
+            }
+        }
+    }
+}
---
+++ WebUI/Controllers/ChairController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.Entities;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class ChairController : Controller
+    {
+        private IChairRepository repository;
+        public int pageSize = 4;
+        public ChairController(IChairRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List(string category, int page = 1)
+        {
+            ChairsListViewModel model = new ChairsListViewModel
+            {
+                Chairs = repository.Chairs
+                    .Where(p => category==null || p.Category==category)
+                    .OrderBy(chair => chair.ChairId)
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = category==null ?
+                    repository.Chairs.Count() : 
+                    repository.Chairs.Where(chair => chair.Category == category).Count()
+                },
+                CurrentCategory = category
+                
+            };
+            return View(model);
+        }
+    }
+}
